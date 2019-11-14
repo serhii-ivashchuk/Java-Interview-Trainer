ecb1aaf1768a6fd28566bc7c2f20591dd70c67b6
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-14 12:19:08 +0100

    Added SpringBootApplication bootstrap class

---
+++ src/main/java/pro/ivashchuk/javainterviewtrainer/app/JavaInterviewTrainer.java
@@ -0,0 +1,10 @@
+package pro.ivashchuk.javainterviewtrainer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInterviewTrainer {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInterviewTrainer.class, args);}
+}
