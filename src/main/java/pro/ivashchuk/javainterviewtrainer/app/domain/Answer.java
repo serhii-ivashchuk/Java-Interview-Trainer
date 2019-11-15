2d6713df90b60234f5a235574940e69a1181409d
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-15 13:50:49 +0100

    Added getters() and setters()

--- src/main/java/pro/ivashchuk/javainterviewtrainer/app/domain/Answer.java
+++ src/main/java/pro/ivashchuk/javainterviewtrainer/app/domain/Answer.java
@@ -15,4 +15,28 @@ public class Answer {
     private String answerText;
 
     private String answerAudioLink;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
+
+    public String getAnswerAudioLink() {
+        return answerAudioLink;
+    }
+
+    public void setAnswerAudioLink(String answerAudioLink) {
+        this.answerAudioLink = answerAudioLink;
+    }
 }
