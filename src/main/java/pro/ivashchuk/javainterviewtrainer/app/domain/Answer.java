100830c85d4f4b065bcc8e542b093f46f25a057c
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-15 13:49:34 +0100

    Added fields

--- src/main/java/pro/ivashchuk/javainterviewtrainer/app/domain/Answer.java
+++ src/main/java/pro/ivashchuk/javainterviewtrainer/app/domain/Answer.java
@@ -1,7 +1,18 @@
 package pro.ivashchuk.javainterviewtrainer.app.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Answer {
-    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String answerText;
+
+    private String answerAudioLink;
+}
