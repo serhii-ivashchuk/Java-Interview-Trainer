548268432ae9891682a03125b14e0a876b0bd07f
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-01-30 19:39:19 +0100

    Added getHomePage() with @GetMapping("/") for binding index page to default route "/"

--- src/main/java/pro/ivashchuk/javainterviewtrainer/app/controller/HomeController.java
+++ src/main/java/pro/ivashchuk/javainterviewtrainer/app/controller/HomeController.java
@@ -1,7 +1,13 @@
 package pro.ivashchuk.javainterviewtrainer.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index";
+    }
 }
