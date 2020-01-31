207feeb1e5b678e5e29d4a0fd67ee67daccb1fe9
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-01-31 20:52:09 +0100

    Add logic for getAllQuestions()

--- src/main/java/pro/ivashchuk/javainterviewtrainer/app/controller/QuestionController.java
+++ src/main/java/pro/ivashchuk/javainterviewtrainer/app/controller/QuestionController.java
@@ -1,15 +1,21 @@
 package pro.ivashchuk.javainterviewtrainer.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pro.ivashchuk.javainterviewtrainer.app.domain.Question;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
 
     @GetMapping
-    public String getAllQuestions(){
+    public String getAllQuestions(Model model){
+        List<Question> questions = jpaQuestionRepository.findAll();
+        model.addAttribute("questions", questions);
         return "all_questions";
     }
 }
