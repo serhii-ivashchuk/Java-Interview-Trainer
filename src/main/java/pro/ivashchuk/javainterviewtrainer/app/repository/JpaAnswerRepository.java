dc290156eb37948eb71527f8c5f34147a05d47b2
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-02-02 18:59:22 +0100

    Add findAll()

--- src/main/java/pro/ivashchuk/javainterviewtrainer/app/repository/JpaAnswerRepository.java
+++ src/main/java/pro/ivashchuk/javainterviewtrainer/app/repository/JpaAnswerRepository.java
@@ -3,5 +3,8 @@ package pro.ivashchuk.javainterviewtrainer.app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.ivashchuk.javainterviewtrainer.app.domain.Answer;
 
+import java.util.List;
+
 public interface JpaAnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findAll();
 }
