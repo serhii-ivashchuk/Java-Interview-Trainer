89035ef563bb488daf4cdd598db7f1472881217e
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-02-02 18:59:50 +0100

    Add findById()

--- src/main/java/pro/ivashchuk/javainterviewtrainer/app/repository/JpaAnswerRepository.java
+++ src/main/java/pro/ivashchuk/javainterviewtrainer/app/repository/JpaAnswerRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pro.ivashchuk.javainterviewtrainer.app.domain.Answer;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JpaAnswerRepository extends JpaRepository<Answer, Long> {
     List<Answer> findAll();
+    Optional<Answer> findById(Long id);
 }
