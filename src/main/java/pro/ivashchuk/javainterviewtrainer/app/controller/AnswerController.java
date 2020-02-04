d213f4d4259f56791aee2188bc3a4c64338db2b9
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-02-04 22:17:16 +0100

    Add logic to getAllAnswers

--- src/main/java/pro/ivashchuk/javainterviewtrainer/app/controller/AnswerController.java
+++ src/main/java/pro/ivashchuk/javainterviewtrainer/app/controller/AnswerController.java
@@ -3,7 +3,9 @@ package pro.ivashchuk.javainterviewtrainer.app.controller;
 public class AnswerController {
 
     @GetMapping
-    public String getAllAnswers () {
+    public String getAllAnswers (Model model) {
+        List<Answer> answers = jpaAnswerRepository.findAll();
+        model.addAttribute("answers", answers);
         return "all_answers";
     }
 }
