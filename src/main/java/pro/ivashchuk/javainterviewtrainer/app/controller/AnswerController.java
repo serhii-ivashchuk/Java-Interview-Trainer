d590da31a90306cdfd8db6cfcf0238b73f135630
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-02-04 22:16:18 +0100

    Add getAllAnswers() with @GetMapping returning "all_answers"

--- src/main/java/pro/ivashchuk/javainterviewtrainer/app/controller/AnswerController.java
+++ src/main/java/pro/ivashchuk/javainterviewtrainer/app/controller/AnswerController.java
@@ -1,4 +1,9 @@
 package pro.ivashchuk.javainterviewtrainer.app.controller;
 
 public class AnswerController {
+
+    @GetMapping
+    public String getAllAnswers () {
+        return "all_answers";
+    }
 }
